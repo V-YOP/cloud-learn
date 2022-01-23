f62dc8f48dc1e743e778aad6b2c75758738e1712
YAPYOPYAP <hentaiisbaka@qq.com>
2022-01-23 14:34:47 +0800

    结束Eureka的部分，下一个是ZooKeeper

--- provider-payment/src/main/java/me/yuuki/provider/payment/controller/PaymentController.java
+++ provider-payment/src/main/java/me/yuuki/provider/payment/controller/PaymentController.java
@@ -6,6 +6,7 @@ import me.yuuki.entity.Payment;
 import me.yuuki.provider.payment.service.PaymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,6 +17,11 @@ public class PaymentController {
     private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
 
     private final PaymentService paymentService;
+
+    // 方便在返回结果中看到究竟是哪个实例被调用了
+    @Value("${server.port}")
+    private String serverPort;
+
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
@@ -23,8 +29,8 @@ public class PaymentController {
     @PostMapping("/create")
     public CommonResult<Void> create(@RequestBody Payment payment) {
         paymentService.create(payment);
-        logger.info("create payment: {}", payment);
-        return CommonResult.empty(200, "成功");
+        logger.info("create payment: {}, server port: {}", payment, serverPort);
+        return CommonResult.empty(200, "成功, server port: " + serverPort);
     }
 
     @GetMapping(value = {"/{id}"})
@@ -32,7 +38,7 @@ public class PaymentController {
         logger.info(paymentService.getPayment(id).toString());
         return paymentService.getPayment(id) // Optional<Payment>
                 .map(CommonResult::success) //Optional<CommonResult<Payment>>
-                .orElse(CommonResult.empty(404,"查无此payment")); // CommonResult<Payment>
+                .orElse(CommonResult.empty(404,"查无此payment, server port: " + serverPort)); // CommonResult<Payment>
     }
 }
 

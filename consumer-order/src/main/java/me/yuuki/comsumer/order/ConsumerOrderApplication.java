b36f96131f3eae56f2abe51d2df009187f7e839b
YAPYOPYAP <hentaiisbaka@qq.com>
2022-02-01 17:15:02 +0800

    添加和配置zookeeper，创建基于docker-compose的zookeeper环境的配置文件

--- consumer-order/src/main/java/me/yuuki/comsumer/order/ConsumerOrderApplication.java
+++ consumer-order/src/main/java/me/yuuki/comsumer/order/ConsumerOrderApplication.java
@@ -2,8 +2,10 @@ package me.yuuki.comsumer.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerOrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerOrderApplication.class, args);
--- provider-payment/src/main/java/me/yuuki/provider/payment/ProviderPaymentApplication.java
+++ provider-payment/src/main/java/me/yuuki/provider/payment/ProviderPaymentApplication.java
@@ -2,12 +2,14 @@ package me.yuuki.provider.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.ConnectionProperties;
 
 import java.sql.Driver;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderPaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderPaymentApplication.class, args);
